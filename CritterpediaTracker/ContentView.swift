942c73545abbbbbba2db6d07412c19ffc698d3b0
Alejandra Gudiel <gud19232@uvg.edu.gt>
2026-07-04 01:14:04 -0600

    Initial Commit

---
+++ CritterpediaTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CritterpediaTracker
+//
+//  Created by Alejandra on 4/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CritterpediaTracker/CritterpediaTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CritterpediaTrackerApp.swift
+//  CritterpediaTracker
+//
+//  Created by Alejandra on 4/07/26.
+//
+
+import SwiftUI
+
+@main
+struct CritterpediaTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
